119909e89961e013a1b8c5a09e0ea93fc8d47876
hudaweis <hdw5329433@163.com>
2016-03-22 21:42:54 +0800

    消息工具类

---
+++ vs-master-common/src/main/java/com/vshuok/es/common/utils/MessageUtils.java
@@ -0,0 +1,27 @@
+package com.vshuok.es.common.utils;
+
+import org.springframework.context.MessageSource;
+
+/** 
+ * <p></p>
+ * @author Hu Dawei  
+ * @version 1.0
+ */
+public class MessageUtils {
+
+	private static MessageSource messageSource;
+	
+	/**
+	 * 根据消息键和参数 获取消息
+	 * 委托给 spring messageSource
+	 * @param code 消息键
+	 * @param args 参数
+	 * @return
+	 */
+	public static String message(String code,Object... args){
+		if(messageSource==null){
+			messageSource=SpringUtils.getBean(MessageSource.class);
+		}
+		return messageSource.getMessage(code, args,null);
+	}
+}
